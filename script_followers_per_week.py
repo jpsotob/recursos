90d204c05448686099d5e4a3c237b8d8f3e948d2
Franko1307 <franciscogonzalez1307@gmail.com>
2018-04-23 23:34:54 -0700

    quitando prints

--- script_followers_per_week.py
+++ script_followers_per_week.py
@@ -10,6 +10,7 @@ consumer_secret = ''
 access_token = '-'
 access_token_secret = ''
 
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
@@ -91,7 +92,6 @@ for candidato in candidatos:
                     cond = False
                     break
 
-        print(len(tweets))
         jsonData[candidato[nombre]]["seguidores"].append([
             now,
             user.followers_count
@@ -104,10 +104,6 @@ for candidato in candidatos:
             now,
             count
         ])
-        
-        
 
-print(jsonData)
 with open(path_to_json, 'w') as outfile:
     json.dump(jsonData, outfile)
-print ("finished")
